7a5dd28bf2c335191561fa5a33f4daa2343055b4
Sodjamts <first_sd@yahoo.com>
2018-02-04 19:46:04 -0600

    some data added

---
+++ src/main/java/mum/edu/Database/DBTasks.java
@@ -0,0 +1,14 @@
+package mum.edu.Database;
+
+public class DBTasks extends Database {
+
+    void connect() throws Exception {
+        conn = DBconnector.getconnector().getconnection();
+    }
+
+    private void checkConn() throws Exception {
+        if (conn == null || conn.isClosed()) {
+            connect();
+        }
+    }
+}
---
+++ src/main/java/mum/edu/Database/DBconnector.java
@@ -0,0 +1,47 @@
+package mum.edu.Database;
+
+import java.sql.*;
+import java.util.Enumeration;
+
+public class DBconnector {
+
+    private Connection conn;
+
+    private DBconnector() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection("jdbc:sqlserver://10.0.0.26:1433;databaseName=telephoneCompany;user=dbms;password=Dbms_123");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static DBconnector connector = new DBconnector();
+
+    public static DBconnector getconnector() {
+        return connector;
+    }
+
+    public Connection getconnection() {
+        return conn;
+    }
+
+    public void closeConnection() {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            Driver driver = null;
+            // clear drivers
+            while (drivers.hasMoreElements()) {
+                try {
+                    driver = drivers.nextElement();
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException ex) {
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+}
